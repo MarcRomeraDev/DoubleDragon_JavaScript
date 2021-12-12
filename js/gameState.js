e8acce9466a2d61a7d91210f00620b2b173c9c36
Ryan Palazón <ryan.palazon@enti.cat>
2021-12-12 15:57:32 +0100

    Screen scrolls on input. Level correctly subdivided

--- js/gameState.js
+++ js/gameState.js
@@ -3,9 +3,9 @@ class gameState extends Phaser.Scene {
         super(
             {
                 key: "gameState"
-                
+
             });
-           
+
     }
     preload() { //carga los assets en memoria
         this.cameras.main.setBackgroundColor("#000000");
@@ -17,14 +17,40 @@ class gameState extends Phaser.Scene {
 
 
     create() { //carga los assets en pantalla desde memoria
-        this.bg1 = this.add.tileSprite(0, 0, config.width, config.height, 'background1').setOrigin(0);
-        this.player = this.physics.add.sprite(config.width / 2, config.height * .8, 'player').setOrigin(.5);
+        this.bg1 = this.add.tileSprite(0, 0, 1015, 192, 'background1').setOrigin(0);
+        this.player = this.physics.add.sprite(config.width / 2, config.height * .7, 'player').setOrigin(.5);
 
-        this.player.body.collideWorldBounds = true;
+        this.player.body.collideWorldBounds = true; //--> Collision with world border walls
+        this.player.body.onWorldBounds = true; //--> On collision event
+
         this.input = this.input.keyboard.createCursorKeys();
+
+        this.mapThreshold = false;
+        this.flipFlop = false;
+        this.numMapSubdivisions = 1015 / config.width;
+        this.count = this.numMapSubdivisions / 4;
     }
 
     update() {
+        this.physics.world.on('worldbounds', (body, up, down, left, right) => {
+        });
+
+        if (this.bg1.tilePositionX < 1015 - (config.width * this.numMapSubdivisions)) {
+            this.bg1.tilePositionX += 1;
+        }
+        else {
+            if (this.input.space.isDown) {
+                if (!this.flipFlop) {
+                    this.numMapSubdivisions -= this.count;
+                    this.flipFlop = true;
+                }
+            }
+        }
+
+        if (this.input.space.isUp) {
+            this.flipFlop = false;
+        }
+
         if (this.input.left.isDown) {
             this.player.body.velocity.x -= 5
             this.player.flipX = true;
