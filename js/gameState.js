3f421841454818ccdf3acddbda1e96551e851b6c
marcRomera <marc.romera.rodriguez@enti.cat>
2021-12-12 23:34:24 +0100

    Animations with feedback sound done

--- js/gameState.js
+++ js/gameState.js
@@ -1,3 +1,5 @@
+
+
 class gameState extends Phaser.Scene {
     constructor() { //crea la escena
         super(
@@ -14,45 +16,61 @@ class gameState extends Phaser.Scene {
         var rutaImg = 'assets/img/';
         var rutaSprites = 'assets/sprites/';
         this.load.image('background1', rutaSprites + 'Mission1BackgroundSprites/1.png');
-        this.load.spritesheet('player', rutaSprites + 'BillySprites/character.png', { frameWidth: 72, frameHeight: 46 });
-
-
+        this.load.spritesheet('player', rutaSprites + 'BillySprites/CharacterSpritesheet.png', { frameWidth: 72, frameHeight: 46 });
+        this.load.audio('bgMusic', 'assets/sounds/music/mission1.ogg');
+        this.load.audio('punch', 'assets/sounds/pucnh.ogg');
     }
 
     create() { //carga los assets en pantalla desde memoria
         this.bg1 = this.add.tileSprite(0, 0, 1015, 192, 'background1').setOrigin(0);
+        this.music = this.sound.add('bgMusic');
+        this.punchSound = this.sound.add('punch');
+        this.music.play();
         this.player = this.physics.add.sprite(config.width / 2, config.height * .7, 'player').setOrigin(.5);
- 
+
         this.player.body.collideWorldBounds = true; //--> Collision with world border walls
         this.player.body.onWorldBounds = true; //--> On collision event
         this.DoOnePunch = true;
-        this.keys = this.input.keyboard.addKeys({
-            a:Phaser.Input.Keyboard.KeyCodes.A
+        this.keyboardKeys = this.input.keyboard.addKeys({
+            a: Phaser.Input.Keyboard.KeyCodes.A
         });
-        this.moveKeys = this.input.keyboard.createCursorKeys();
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.attackFlipFlop = false;
         this.flipFlop = false;
         this.numMapSubdivisions = 1015 / config.width;
         this.count = this.numMapSubdivisions / 4;
         this.canAdvance = false;
         this.createPlayerAnims();
-       
-       // this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A, false)
-       // this.input.keyboard.on("keydown_A", (e) => {
-       //     this.attackPlayerManager();
+        this.isAttacking = false;
+        this.player.setFrame(1);
+
+        // this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A, false)
+        // this.input.keyboard.on("keydown_A", (e) => {
+        //     this.attackPlayerManager();
         //  });
         //var combo = this.input.keyboard.createCombo()
-       //  this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        //  this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         //this.keyB = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
     }
-    createPlayerAnims() 
-    {
+    createPlayerAnims() {
         this.anims.create({
             key: 'run',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+            frames: this.anims.generateFrameNumbers('player', { start: 2, end: 3 }),
             frameRate: 5,
             repeat: -1
+        });
+        this.anims.create({
+            key: 'rightAttack',
+            frames: this.anims.generateFrameNumbers('player', { start: 5}),
+            duration: 1000,
+            repeat: 0
+        });
+        this.anims.create({
+            key: 'leftAttack',
+            frames: this.anims.generateFrameNumbers('player', { start: 4 }),
+            duration: 1000,
+            repeat: 0
         });
     }
 
@@ -64,63 +82,69 @@ class gameState extends Phaser.Scene {
     }
 
     movePlayerManager() {
-        this.player.setVelocity(0, 0);
 
-        if (this.input.down.isDown) { // down
-            this.player.setVelocityY(gamePrefs.playerSpeed);
-            this.player.play('run', true);
-        }
-        else if (this.input.up.isDown) { // up
-            if (this.player.body.y > config.height / 2 + 10) {
-                this.player.setVelocityY(-gamePrefs.playerSpeed);
+        this.player.setVelocity(0, 0);
+        if (!this.isAttacking) 
+        {
+            if (this.cursorKeys.down.isDown) { // down
+                this.player.setVelocityY(gamePrefs.playerSpeed);
                 this.player.play('run', true);
             }
-        }
-
-        if (this.input.left.isDown) { //left
-            this.player.setVelocityX(-gamePrefs.playerSpeed);
-            this.player.play('run', true);
-            this.player.flipX = true;
-        }
-        else if (this.input.right.isDown) { // right
-            this.player.setVelocityX(gamePrefs.playerSpeed);
-            this.player.play('run', true);
-            this.player.flipX = false;
-
-            if (this.canAdvance && (this.bg1.tilePositionX < 1015 - (config.width * this.numMapSubdivisions))) {
-                if (this.player.body.x > config.width * 2 / 3) {
-                    this.bg1.tilePositionX += .3; //--> Background scroll speed
-                    this.player.body.velocity.x = 0.001;
+            else if (this.cursorKeys.up.isDown) { // up
+                if (this.player.body.y > config.height / 2 + 10) {
+                    this.player.setVelocityY(-gamePrefs.playerSpeed);
+                    this.player.play('run', true);
                 }
             }
-            else {
-                this.canAdvance = false;
-                this.flipFlop = false;
-            }
-        }
 
-        if (this.player.body.velocity.x == 0 && this.player.body.velocity.y == 0)
-            this.player.setFrame(0);
+            if (this.cursorKeys.left.isDown) { //left
+                this.player.setVelocityX(-gamePrefs.playerSpeed);
+                this.player.play('run', true);
+                this.player.flipX = true;
+            }
+            else if (this.cursorKeys.right.isDown) { // right
+                this.player.setVelocityX(gamePrefs.playerSpeed);
+                this.player.play('run', true);
+                this.player.flipX = false;
+                if (this.canAdvance && (this.bg1.tilePositionX < 1015 - (config.width * this.numMapSubdivisions))) {
+                    if (this.player.body.x > config.width * 2 / 3) {
+                        this.bg1.tilePositionX += .3; //--> Background scroll speed
+                        this.player.body.velocity.x = 0.001;
+                    }
+                }
+                else {
+                    this.canAdvance = false;
+                    this.flipFlop = false;
+                }
+            }
+
+            if (this.player.body.velocity.x == 0 && this.player.body.velocity.y == 0)
+                this.player.setFrame(1);
+        }
     }
     attackPlayerManager() {
-       if(this.keys.a.isDown)
-       {
-           if(this.DoOnePunch)
-           {
-               this.DoOnePunch= false;
-                if(this.attackFlipFlop)
-                    this.player.setFrame(26);
-                else{
-                    this.player.setFrame(37);
-                }
-            this.attackFlipFlop = !this.attackFlipFlop;   
-           }   
-       }
-       else if(this.keys.a.isUp)
-       {
-            this.DoOnePunch = true; 
-       }
-     
+        if(Phaser.Input.Keyboard.JustDown(this.keyboardKeys.a) && !this.isAttacking)
+        {
+                if (this.attackFlipFlop)
+                        this.player.setFrame(4)
+                else 
+                     this.player.setFrame(5);
+
+                this.attackFlipFlop = !this.attackFlipFlop;
+              
+                this.player.stop();
+                this.punchSound.play();
+                this.isAttacking = true;
+        }
+        if(Phaser.Input.Keyboard.JustUp(this.keyboardKeys.a))
+        {
+            this.isAttacking = false;
+        }
+
+    }
+    resetAttackTimer()
+    {
+
     }
 
     update() {
@@ -129,10 +153,10 @@ class gameState extends Phaser.Scene {
         this.attackPlayerManager();
         this.physics.world.on('worldbounds', (body, up, down, left, right) => {
         });
-        
+
 
         //INPUT TO ACTIVATE FLAG TO SCROLL BACKGROUND
-        if (this.moveKeys.space.isDown) {
+        if (this.cursorKeys.space.isDown) {
             if (!this.flipFlop) {
                 this.numMapSubdivisions -= this.count;
                 this.flipFlop = true;
