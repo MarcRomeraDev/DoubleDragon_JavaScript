8f2cfd1939be048946c42bfcb3f940b67063cbf6
Ryan Palazón <ryan.palazon@enti.cat>
2022-01-05 15:30:43 +0100

    level 2 code clean

--- js/level2.js
+++ js/level2.js
@@ -23,7 +23,17 @@ class level2 extends Phaser.Scene {
         this.maxY = config.height / 3 + 5;
         this.minY = config.height / 2 + 15;
 
-        this.gameTimer = this.time.addEvent({ delay: 1000, callback: function () { this.gameTime--; }, callbackScope: this, loop: true });
+        this.gameTimer = this.time.addEvent({
+            delay: 1000, callback: function () {
+                this.gameTime--;
+                this.timeText.setText('TIME ' + this.gameTime);
+                if (this.gameTime < 0) {
+                    this.player.health = 0;
+                    this.checkPlayerHealth();
+                }
+            },
+            callbackScope: this, loop: true
+        });
 
         //STORES EVERY INPUT KEY WE NEED
         this.keyboardKeys = this.input.keyboard.addKeys({
@@ -49,17 +59,14 @@ class level2 extends Phaser.Scene {
             }
         }
 
-        this.createPlayerAnims();
         this.createBackgroundAnim();
-
-        //this.createWilliamsAnims();
         //this.physics.add.overlap(this.player.attackHitbox, this.waveSystem.enemies, this.waveSystem.dmgEnemy, null, this.waveSystem);
 
         this.playerText = this.add.text(20, config.height - 20, '1P', { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //player
         this.expText = this.add.text(20, config.height - 12, this.player.exp, { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //exp
-        this.timeText = this.add.text(config.width / 2 + 10, config.height - 12, 'TIME ', { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //game time
+        this.timeText = this.add.text(config.width / 2 + 10, config.height - 12, 'TIME ' + this.gameTime, { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //game time
         this.scoreText = this.add.text(config.width - 60, config.height - 12, '1P ', { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //score text
-        this.scoreNumbersText = this.add.text(config.width - 25, config.height - 12, this.player.score, { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //score num
+        this.scoreNumbersText = this.add.text(config.width - 25, config.height - 12, '00', { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //score num
         this.highScoreText = this.add.text(config.width - 60, config.height - 20, 'HI ', { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //highscore text
         this.highScoreNumbersText = this.add.text(config.width - 25, config.height - 20, this.player.highScore, { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //highscore num
         this.lifesText = this.add.text(config.width / 2 + 14, config.height - 20, 'P-2', { fontFamily: 'dd_font', fontSize: '7px' }).setOrigin(0.5).setSize(); //game time
@@ -67,7 +74,6 @@ class level2 extends Phaser.Scene {
 
     update() {
         this.updateLevel();
-        this.updateGameTimer();
         this.player.updatePlayer();
         this.updateConveyorBelt();
 
@@ -90,15 +96,9 @@ class level2 extends Phaser.Scene {
     }
 
     //#region  UPDATES
-    updateGameTimer() {
-        if (this.player.lifes >= 0) {
-            this.timeText.setText('TIME ' + this.gameTime);
-        }
-    }
-
     updateExp(exp) {
-        this.exp += exp;
-        this.expText.setText(this.exp.toString());
+        this.player.exp += exp;
+        this.expText.setText(this.player.exp);
     }
 
     updateScore() {
@@ -113,10 +113,10 @@ class level2 extends Phaser.Scene {
     }
 
     updateLevel() {
-        if (this.exp >= 1000) {
+        if (this.player.exp >= 1000) {
             this.player.level++;
-            this.exp = 0;
-            this.expText.setText(this.exp.toString());
+            this.player.exp = 0;
+            this.expText.setText(this.player.exp.toString());
             this.hearts[this.player.level - 1].visible = true;
         }
     }
@@ -147,39 +147,6 @@ class level2 extends Phaser.Scene {
 
         this.add.sprite(config.width, 192, 'background2.3').setOrigin(1).play('level2_background_change');
     }
-
-    createPlayerAnims() {
-        this.anims.create({
-            key: 'run',
-            frames: this.anims.generateFrameNumbers('player', { start: 2, end: 3 }),
-            frameRate: 5,
-            repeat: -1
-        });
-    }
-
-    createWilliamsAnims() {
-        this.anims.create({
-            key: 'williamsrun',
-            frames: this.anims.generateFrameNumbers('williams', { start: 0, end: 2 }),
-            frameRate: 5,
-            yoyo: true,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'williamstakeDmg',
-            frames: this.anims.generateFrameNumbers('williams', { start: 5, end: 7 }),
-            frameRate: 5,
-            yoyo: true,
-            repeat: 0
-        });
-        this.anims.create({
-            key: 'williamsdie',
-            frames: this.anims.generateFrameNumbers('williams', { start: 8, end: 9 }),
-            frameRate: 5,
-            yoyo: false,
-            repeat: 0
-        });
-    }
     //#endregion
 
     //INITIALIZES PLAYER INFO WITH DATA RECIEVED ON SCENE CREATION
@@ -208,7 +175,9 @@ class level2 extends Phaser.Scene {
             this.player.kill();
             this.music.play();
             if (this.player.lifes >= 0) {
+                this.gameTime = 200;
                 this.lifesText.setText("P-" + this.player.lifes);
+                this.timeText.setText('TIME ' + this.gameTime);
                 for (var i = 0; i < this.player.health; i++) {
                     this.health[i].visible = true;
                 }
@@ -216,6 +185,7 @@ class level2 extends Phaser.Scene {
             else {
                 this.scene.pause();
                 this.music.stop();
+                this.gameTimer.remove();
                 this.timeText.setText("GAME OVER");
                 this.gameOverMusic.on('complete', function () { this.scene.start('menu'); }, this);
                 this.gameOverMusic.play();
